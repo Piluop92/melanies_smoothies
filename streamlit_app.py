de9ba28071371baed503fb0cd1c2ecac4aa60bd0
Piluop92 <claudio@thevantora.com>
2025-05-01 15:53:03 -0500

    Update streamlit_app.py
    Connected changes needed bc of move from sis to SNiS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
@@ -11,6 +10,9 @@ st.write(
 
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+
+cnx = st.conncetion("snowflake")
+session = cnx.session()
 
 if my_dataframe:
     editable_df = st.data_editor(my_dataframe)   
